d8b6c8449c9fc651968c8ac48b8fa764ec5c38bd
Tijani Fulani <Fulani07@gmail.com>
2021-04-28 17:45:24 +0100

    email checker done

---
+++ index.js
@@ -0,0 +1,41 @@
+/*
+Description
+These exercises are for you to practise arrays and loops. So keep those in mind, and see if you can figure out how to apply them to each exercise.
+
+Instructions
+
+Email checker
+- Make a list of emails
+- Ask the user for their email
+- Check if their email is listed
+- Output a message granting them access if the email is listed
+
+Dice roller
+- Generate a random number from 1 to 6, for both the player and the computer
+- Determine the winner, based on who gets the highest score and let the user know
+- Make it a best of 5!
+
+Tips
+- If we have to compare something, "what" do we need to do the comparison?
+- Try to write comments first explaining what you plan to do step by step
+
+Have fun!
+*/
+
+const emails = ["tijani@gmail.com", "nico@gmail.com", 'lars@gmail.com', "rico@gmail.com", "james@gmail.com" ]
+
+
+ let userEmail = prompt("What is your email?")
+
+
+
+
+ if(emails.includes(userEmail) === true) {
+  console.log("Welcome to the email checker")
+ } else {
+  
+  userEmail = prompt("Sorry but thats the wrong email. Please type in your email again:")
+ }
+
+  console.log(userEmail)
+ console.log(emails)
